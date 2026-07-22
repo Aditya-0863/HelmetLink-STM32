98ca32382ffcb447e0f37545c52d9044277adb51
Aditya <adityaanil7812@gmail.com>
2026-07-22 21:40:17 +0530

    Update led-blink.c

--- firmware/01-led-blink/Core/Src/led-blink.c
+++ firmware/01-led-blink/Core/Src/led-blink.c
@@ -1,24 +1,26 @@
 #include <stdint.h>
 
-void delay(volatile uint32_t count){
+void delay(volatile uint32_t count)
+{
 	while(count--);
 }
-#define rcc (*(volatile uint32_t*) 0x40021034)
-#define moder (*(volatile uint32_t*) 0x50000000)
-#define odr (*(volatile uint32_t*) 0x50000014)
+
+#define rcc_iopenr (*(volatile uint32_t*) 0x40021034)
+#define gpioa_moder (*(volatile uint32_t*) 0x50000000)
+#define gpioa_odr (*(volatile uint32_t*) 0x50000014)
 
 int main(void){
-	rcc |= 0x1U<<0;
-	moder &= ~(0x3U<<10);
-	moder |= 0x1U<<10;
+	rcc_iopenr |= 0x1U<<0;
+	gpioa_moder &= ~(0x3U<<10);
+	gpioa_moder |= 0x1U<<10;
 
 	while(1)
 	    {
-	        odr |= 0x1U<<5;
+	        gpioa_odr |= 0x1U<<5;
 
 	        delay(500000);
 
-	        odr &=~(0x1U<<5);
+	        gpioa_odr &=~(0x1U<<5);
 
 	        delay(500000);
 	    }
